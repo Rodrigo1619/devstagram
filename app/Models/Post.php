52485c3925cb3c0951ef24f0d98278190c76b806
Rodrigo1619 <00067920@uca.edu.sv>
2023-12-26 14:30:31 -0600

    feat: showing if a user liked a post or not

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -26,4 +26,11 @@ class Post extends Model
     public function likes(){
         return $this->hasMany(Like::class);
     }
+
+    //ver si un usuario ya dio me gusta y que no hayan duplicados
+    public function checkLikes(User $user){
+        //ya tenemos una relacion con likes, lo usamos para checkear si ya hay un id de usuario en nuestra tabla de Likes
+        //en la columna de 'user_id'
+        return $this->likes->contains('user_id', $user->id);
+    }
 }
